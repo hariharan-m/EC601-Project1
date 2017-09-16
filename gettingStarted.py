a6fc7a6fd1626ce50f4c82b1b7779c11c9581baf
helloimlixin <31867524+helloimlixin@users.noreply.github.com>
2017-09-16 04:03:19 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -54,3 +54,11 @@ print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
 
 add_and_triple = adder_node * 3.
 print(sess.run(add_and_triple, {a: 3, b: 4.5}))
+
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+
+init = tf.global_variables_initializer()
+sess.run(init)
